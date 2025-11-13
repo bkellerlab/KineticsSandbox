b17fa1bf9ccf7fb90681137aab332979c7615ead
anyschaefer <schaefej51@zedat.fu-berlin.de>
2025-11-13 12:37:31 +0100

    Updated system class to capture random numbers, bias force and path entropy for reweighting on the fly.

--- system/system.py
+++ system/system.py
@@ -16,7 +16,7 @@ import scipy.constants as const
 #------------------------------------------------
 class D1():
     # intiialize class
-    def __init__(self, m, x, v, T, xi, dt, h): 
+    def __init__(self, m, x, v, T, xi, dt, h, eta_k=None, eta_l=None, bias_force=None, logM=None): 
         """
         Initialize the class for a 1-dimensional system based on the given parameters.
 
@@ -28,6 +28,11 @@ class D1():
             - xi (float): collision frequency in units of 1/ps
             - dt (float): time step in units of ps
             - h (float): discretization interval for numerical forces and hessian
+            - eta_k (float): random number drwan for propagation 
+                             Note: in BOAOB/ OBABO/BP == eta1; in AOBOA combined eta
+            - ela_l (float): second random number for BOAOB/ OBABO/BP integrator
+            - bias_force (func): function to describe bias potential force
+            - logM (float): path entropy $1/D \Delta S$ in eq 28 J. Phys. Chem. B 2024, 128, 6014−6027 
         """
         
         # Assign parameters
@@ -38,6 +43,10 @@ class D1():
         self.xi = xi
         self.dt = dt
         self.h = h
+        self.eta_k = eta_k
+        self.eta_l = eta_l
+        self.bias_force = bias_force
+        self.logM = logM
         
         # calculate some combined parameters
         # mass-weighted friction
